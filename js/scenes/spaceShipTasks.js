068be97147bc25fd2a78146b2a02155d9106b0de
iamomar <email@email.com>
2023-05-14 23:58:32 -0400

    added spaceship model and movement

--- js/scenes/spaceShipTasks.js
+++ js/scenes/spaceShipTasks.js
@@ -1,3 +1,6 @@
+import * as global from "../global.js";
+import { quat } from "../render/math/gl-matrix.js";
+import { Gltf2Node } from "../render/nodes/gltf2.js";
 import { g2 } from "../util/g2.js";
 import * as cg from "../render/core/cg.js";
 import { controllerMatrix, buttonState, joyStickState } from "../render/core/controllerInput.js";
@@ -127,7 +130,17 @@ export let updateView = (event) => {
 
 export const init = async model => {
   model.setTable(false);
+  model.setRoom(false);
+  model.setTable(false);
+  let moveSpeed = .05;
+  let rotateSpeed = 1;
+  let gltf1 = new Gltf2Node({ url: './media/gltf/SpaceShip/scene.gltf' });
+  let rotation1 = quat.create();
+  let sceneRotation = quat.create();
 
+ gltf1.translation = [0, -1, 0];
+
+ global.gltfRoot.addNode(gltf1);
   /**
    * ===================
    * CROQUET SETUP
@@ -354,6 +367,40 @@ export const init = async model => {
 
   model.animate(() => {
     
+    let lx = moveSpeed*joyStickState.left.x;
+      let ly = moveSpeed*joyStickState.left.y;
+      let rx = joyStickState.right.x;
+      let ry = joyStickState.right.y;
+
+      let Rx = sceneRotation[1];
+      let Ry = sceneRotation[3];
+      if(rx < 0){
+         quat.rotateY(sceneRotation, sceneRotation, -rotateSpeed*0.05);
+      }
+      else if(rx > 0){
+         quat.rotateY(sceneRotation, sceneRotation, rotateSpeed*0.05);
+      }
+      
+      if(Math.abs(Rx) < .3){
+         gltf1.translation[2] -= ly;
+         gltf1.translation[0] -= lx;
+      }
+      else if(Math.abs(Rx) > .9){
+         gltf1.translation[2] += ly;
+         gltf1.translation[0] += lx;
+      }
+      else if(Rx*Ry <0){//left
+         gltf1.translation[0] -= ly;
+         gltf1.translation[2] += lx;
+      }
+      else{//right
+         gltf1.translation[0] += ly;
+         gltf1.translation[2] -= lx;
+      }
+ 
+      global.gltfRoot.rotation = sceneRotation; // Apply the scene rotation to the root node
+      gltf1.rotation = rotation1;
+
     if (hudMenu != null){
       // hudMenu.hud().scale(1, 1, .0001)
     }
